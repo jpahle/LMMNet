3cfc58b58149cdbc028754b417693743957c5020
EC2 Default User <ec2-user@ip-172-16-54-177.us-east-2.compute.internal>
2020-05-13 14:13:49 +0000

    feat: add function to compute L2 norm

--- predict.py
+++ predict.py
@@ -15,3 +15,17 @@ def predict_fn(x, t, model):
     dat = x.reshape(1,-1)
     dat = tf.convert_to_tensor(dat, dtype=tf.float32)
     return np.ravel(model.predict(dat))
+
+def compute_MSE(pred, data, index):
+    """
+    A simple function to compute the L2 norm between pred and data
+    Data and predictions are assumed to be multi-dimensional
+    
+    Args:
+    * pred -- the predictions
+    * data -- the ground truth
+    * index -- the component index of the multi-dimensional data to evaluate
+    """
+    pred_array = np.array(pred)
+    data = np.squeeze(data)
+    return np.linalg.norm(data[:,index] - pred_array[:,index], 2)/np.linalg.norm(data[:,index], 2)
