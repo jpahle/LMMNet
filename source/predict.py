237c957bc348c944075fd071d175e35ded765b47
Kevin Siswandi <siswandi.kevin@gmail.com>
2020-05-22 10:12:36 +0000

    feat: add results for step size scan

--- source/predict.py
+++ source/predict.py
@@ -1,4 +1,4 @@
-from model import lmmNet
+from .model import lmmNet
 import numpy as np
 import tensorflow as tf
 
--- source/train.py
+++ source/train.py
@@ -1,12 +1,12 @@
 from .model import lmmNet
-from predict import *
+from .predict import *
 import numpy as np
 import tensorflow as tf
 from scipy.integrate import odeint
 import argparse
 import pickle
 
-def bier(x,t, params=None):
+def f_bier(x,t, params=None):
     """
     2-D Yeast Glycolytic oscillator model
     
@@ -33,6 +33,18 @@ def bier(x,t, params=None):
     r2 = Vin - k1 * x[0] * x[1] #G
     
     return np.ravel(np.array([r1, r2]))
+
+def f_cubic(x,t):
+    """
+    Return the derivatives (RHS of the ODE)
+    This is a linear system with the form f = A x
+    Args:
+    x -- a 2 x 1 vector of measurements
+    """
+    A = np.array([[-0.1, 2], [-2,-0.1]]) # 2 x 2
+
+    return np.ravel(np.matmul(A,x.reshape(-1, 1)**3))
+
 
 def create_training_data(start_time, end_time, step_size, f, x0, integrator='scipy', noise_strength=0):
     """
@@ -88,12 +100,12 @@ if __name__ == "__main__":
         t0, T, h = 0, 500, 0.2 #seconds
         x0 = np.array([4, 3]) #initial conditions: ATP = 4, G = 3 -- default Bier model
         params = {'Vin': 0.36, 'k1': 0.02, 'kp':4, 'km':15} # damped oscillation
-        f = lambda x, t: bier(x, t, params)
+        f = lambda x, t: f_bier(x, t, params)
     elif args.system == 'Cubic':
         # 2-D Cubic settings
         t0, T, h = 0, 25, 0.01
         x0 = np.array([2,0]) # initial conditions -- default cubic problem
-        f = cubic # the system to study
+        f = f_cubic # the system to study
     elif args.system == 'Lorenz':
         # 3-D Lorenz settings
         x0 = np.array([-8.0, 7.0, 27])
