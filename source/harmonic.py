93c18f8d9c28174eaa223c6b438253f3f026739c
Kevin Siswandi <siswandi.kevin@gmail.com>
2020-07-26 19:51:18 +0200

    feat: add simulate custom for harmonic oscillator

--- source/harmonic.py
+++ source/harmonic.py
@@ -25,3 +25,17 @@ def simulate_default():
     time_points, cubic_data = train_lmmNet.create_training_data(t0, T, h, f_cubic, x0)
     
     return time_points, cubic_data
+
+def simulate_custom(tstart=0, tend=25, step=.01, xinit=2, yinit=0):
+    """
+    Simulate the default 2-D damped harmonic oscillator cubic dynamics
+    """
+    
+    # time domain
+    t0, T, h = tstart, tend, step
+    
+    x0 = np.array([xinit,yinit]) # initial conditions -- default cubic problem
+    
+    time_points, cubic_data = train_lmmNet.create_training_data(t0, T, h, f_cubic, x0)
+    
+    return time_points, cubic_data
