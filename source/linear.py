6eb2289dd27dddaa13b63d940e46814a1ad3dbeb
Kevin Siswandi <siswandi.kevin@gmail.com>
2020-07-29 16:43:39 +0200

    feat: add linear systems

--- source/linear.py
+++ source/linear.py
@@ -5,9 +5,26 @@ def f_linear(x,t):
     """
     Return the derivatives (RHS of the ODE)
     This is a linear system with the form f = A x
+    
     Args:
     x -- a 2 x 1 vector of measurements
     """
-    A = np.array([[-0.1, 2], [-2,-0.1]]) # 2 x 2
+    A = np.array([[-0.1, -2, 0], [2,-0.1, 0], [0, 0, -0.3]]) # 3 x 3
 
-    return np.ravel(np.matmul(A,x.reshape(-1, 1)**3))
+    return np.ravel(np.matmul(A,x.reshape(-1, 1)))
+
+def simulate_default():
+    """
+    Simulate the 3-D oscillator with linear dynamics
+    using the default parameters
+    """
+    
+    tfirst, tlast, step = 0, 50, 0.01
+    
+    x0 = np.array([2,0,1]) #initial values
+    
+    time_points, data = train_lmmNet.create_training_data(tfirst, tlast, step, f_linear, x0)
+    
+    return time_points, data
+    
+
