012e842b30ba6451a586ab8a0666878861dc4396
Kevin Siswandi <siswandi.kevin@gmail.com>
2020-08-03 17:11:18 +0200

    feat: start lorenz system

---
+++ source/lorenz.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+def lorenz(x,t):
+    """
+    The system of ODEs for the nonlinear Lorenz system with default parameters
+    
+    Arguments:
+    x --  a list with three elements corresponding to the three variables
+    t -- time
+    
+    Return:
+    A numpy array containing the derivatives
+    """
+    
+    sigma = 10.0
+    beta = 8.0/3.0
+    rho = 28.0
+
+    r1 = sigma * (x[1] - x[0])
+    r2 = x[0] * (rho - x[2]) - x[1]
+    r3 = x[0] * x[1] - beta * x[2]
+
+    return np.array([r1, r2, r3])
