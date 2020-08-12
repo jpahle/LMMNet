ee8fa6e1752d7726ebd0dd96c81b99b977971ddc
Kevin Siswandi <siswandi.kevin@gmail.com>
2020-08-12 17:28:37 +0200

    feat: add bier model script

---
+++ source/bier.py
@@ -0,0 +1,55 @@
+import numpy as np
+from scipy.integrate import odeint
+
+def f_bier(x,t, params=None):
+    """
+    2-D Yeast Glycolytic oscillator model
+    
+    Args:
+        x -- a 2 x 1 vector of measurements
+        t -- time, ignored
+        
+    Return:
+        A numpy array containing the derivatives
+    """
+    if params == None:
+        # default parameter values
+        Vin = 0.36
+        k1 = 0.02
+        kp = 6
+        km = 12
+    else:
+        Vin = params['Vin']
+        k1 = params['k1']
+        kp = params['kp']
+        km = params['km']
+    
+    r1 = 2 * k1 * x[0] * x[1] - kp * x[0]/(x[0] + km) # ATP
+    r2 = Vin - k1 * x[0] * x[1] #G
+    
+    return np.ravel(np.array([r1, r2]))
+
+def simulate_default():
+    
+    # generate data
+    t0 = 0
+    T = 1000 # in seconds
+    h = 0.2
+    time_points = np.arange(t0, T, h)
+    x0 = np.array([4, 3]) #initial conditions: ATP = 4, G = 3
+    
+    bier_data = odeint(f_bier, x0, time_points)
+    
+    return time_points, bier_data
+
+        
+def simulate_custom(t0 = 0, T = 1000, h=0.2, params={'Vin': 0.36, 'k1': 0.02, 'kp':4, 'km':15}):
+    """
+    The default parameters in the params argument are for damped oscillation
+    """
+    
+    x0 = np.array([4, 3]) #initial conditions: ATP = 4, G = 3 -- default Bier model
+    time_points = np.arange(t0, T, h)
+    bier_data = odeint(lambda x, t: f_bier(x, t, params), x0, time_points)
+    
+    return time_points, bier_data
--- source/train_lmmNet.py
+++ source/train_lmmNet.py
@@ -6,35 +6,6 @@ from scipy.integrate import odeint
 import argparse
 import pickle
 
-def f_bier(x,t, params=None):
-    """
-    2-D Yeast Glycolytic oscillator model
-    
-    Args:
-        x -- a 2 x 1 vector of measurements
-        t -- time, ignored
-        
-    Return:
-        A numpy array containing the derivatives
-    """
-    if params == None:
-        # default parameter values
-        Vin = 0.36
-        k1 = 0.02
-        kp = 6
-        km = 12
-    else:
-        Vin = params['Vin']
-        k1 = params['k1']
-        kp = params['kp']
-        km = params['km']
-    
-    r1 = 2 * k1 * x[0] * x[1] - kp * x[0]/(x[0] + km) # ATP
-    r2 = Vin - k1 * x[0] * x[1] #G
-    
-    return np.ravel(np.array([r1, r2]))
-
-
 
 def create_training_data(start_time, end_time, step_size, f, x0, integrator='scipy', noise_strength=0):
     """
@@ -101,12 +72,6 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
     
-    if args.system == 'Bier':
-        # 2-D Bier settings
-        t0, T, h = 0, 500, 0.2 #seconds
-        x0 = np.array([4, 3]) #initial conditions: ATP = 4, G = 3 -- default Bier model
-        params = {'Vin': 0.36, 'k1': 0.02, 'kp':4, 'km':15} # damped oscillation
-        f = lambda x, t: f_bier(x, t, params)
 
     elif args.system == 'Ruoff':
         # 7-D Glycolysis settings
