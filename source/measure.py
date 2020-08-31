14312d0eb60d66557c6359799f03d3ee50b14618
Kevin Siswandi <siswandi.kevin@gmail.com>
2020-08-31 23:26:46 +0200

    feat: add function for report linear onestep

--- source/measure.py
+++ source/measure.py
@@ -185,3 +185,65 @@ def report_linear_lmmnet(metric_function):
         plt.show()
         
     return error_list
+
+
+
+def report_linear_onestep(metric_function):
+    
+    error_list = []
+    
+    time_points, test_data = linear.simulate_default()
+
+    feature_list = [] # here we do not have external time-series or control variables
+    target_list = ['x_component', 'y_component', 'z_component']
+    df_test = create_data(test_data, time_points, cols=target_list, num=2)
+
+
+    for _ in range(10):
+        # generate data with random initial conditions
+        xi = np.random.uniform(1, 4, 1)[0]
+        yi = np.random.uniform(0, 4, 1)[0]
+        zi = np.random.uniform(1, 2, 1)[0]
+        time_points, cubic_data = linear.simulate_custom(xinit=xi, yinit=yi, zinit=zi)
+
+        df = create_data(cubic_data, time_points, cols=target_list, num=0)
+        df_train = train_onestep.generate_dataset(df, [0],feature_list, target_list, n_dim=2500)
+        
+        rf_model = RandomForestRegressor(n_estimators=20)
+        figure_path = './plots/'
+        rf_dict, score_dict = train_onestep.train_classic(df_train, rf_model, plot=False,model_type='random_forest', figure_path=figure_path)
+        time_points, predictions = predict_onestep.predict_integrate(df_test, df, rf_dict, target_list, feature_list, title='test', plot=False,model_type='random_forest', subplots=(3,1), bio=False)
+        predictions = predictions.to_numpy()
+        
+        if metric_function == "wasserstein":
+            e1 = wasserstein_distance(predictions[:,0], test_data[0,:,0])
+            e2 = wasserstein_distance(predictions[:,1], test_data[0,:,1])
+            e3 = wasserstein_distance(predictions[:,2], test_data[0,:,2])
+            
+        elif metric_function == "dtw":
+            e1, _ = fastdtw(predictions[:,0], test_data[0,:,0], dist=euclidean)
+            e2, _ = fastdtw(predictions[:,1], test_data[0,:,1], dist=euclidean)
+            e3, _ = fastdtw(predictions[:,2], test_data[0,:,2], dist=euclidean)
+            e1 /= np.linalg.norm(test_data[0,:,0], 2)**2
+            e2 /= np.linalg.norm(test_data[0,:,1], 2)**2
+            e3 /= np.linalg.norm(test_data[0,:,2], 2)**2
+            
+        elif metric_function == "mse":
+            e1 = predict_lmmNet.compute_MSE(predictions, test_data[0], 0)
+            e2 = predict_lmmNet.compute_MSE(predictions, test_data[0], 1)
+            e3 = predict_lmmNet.compute_MSE(predictions, test_data[0], 2)
+        error_list.append((e1, e2, e3))
+        
+        # plot
+        plt.figure(figsize=(20, 10))
+        plt.plot(time_points, test_data[0,:,0], 'r.', label='x_1')
+        plt.plot(time_points, test_data[0,:,1], 'y.', label='x_2')
+        plt.plot(time_points, test_data[0,:,2], 'g.', label='x_3')
+        plt.plot(time_points, predictions[:,0], 'b--', label='predicted dynamics')
+        plt.plot(time_points, predictions[:,1], 'b--')
+        plt.plot(time_points, predictions[:,2], 'b--')
+        plt.title(str(xi) + " " + str(yi) + " " + str(zi))
+        plt.legend()
+        plt.show()
+        
+    return error_list
