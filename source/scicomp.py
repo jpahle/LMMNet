712f7190d83b9671cf1ca501ae353ded91f3e441
Kevin Siswandi <siswandi.kevin@gmail.com>
2020-09-11 12:34:44 +0200

    feat: add notebooks

--- source/scicomp.py
+++ source/scicomp.py
@@ -6,3 +6,14 @@ def normalize(v):
     """
     
     return v/np.linalg.norm(v)
+
+def get_mse(pred, dat):
+    e1 = predict_lmmNet.compute_MSE(pred, dat, 0)
+    e2 = predict_lmmNet.compute_MSE(pred, dat, 1)
+    e3 = predict_lmmNet.compute_MSE(pred, dat, 2)
+    e4 = predict_lmmNet.compute_MSE(pred, dat, 3)
+    e5 = predict_lmmNet.compute_MSE(pred, dat, 4)
+    e6 = predict_lmmNet.compute_MSE(pred, dat, 5)
+    e7 = predict_lmmNet.compute_MSE(pred, dat, 6)
+    
+    return (e1, e2, e3, e4, e5, e6, e7)
